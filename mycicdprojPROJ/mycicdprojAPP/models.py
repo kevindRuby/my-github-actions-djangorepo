fdaa1f12a1d9e89507e95f13382cc90d2634f134
Kevin D'Andrade <Kevin D'Andrade>
2023-08-06 16:30:44 -0400

    my third commit

--- mycicdprojPROJ/mycicdprojAPP/models.py
+++ mycicdprojPROJ/mycicdprojAPP/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
-#MY TEST OF GITHUB COMMIT
+class Post(models.Model):
+	title = models.CharField(max_length=255)
+	author = models.CharField(max_length=255)
+	slug = models.SlugField(max_length=255)
+
+	def __str__(self):
+		return self.title
--- mycicdprojPROJ/mycicdprojAPP/tests.py
+++ mycicdprojPROJ/mycicdprojAPP/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from . models import Post
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+
+    # Brings database and fields in for testing
+    def setUp(self):
+        self.mycicdprojAPP = Post.objects.create(title='django', author='django', slug='django')
+
+    def test_post_model(self):
+        d = self.mycicdprojAPP
+        # Test if data was entered in db successful
+        self.assertTrue(isinstance(d, Post))
+        # Test if data was entered in db successful
+        self.assertEqual(str(d), 'django')
