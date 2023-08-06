4673858f8e6e3a03782ee91fe0115441d825bb32
Kevin D'Andrade <Kevin D'Andrade>
2023-08-06 19:12:47 -0400

    my 40th commit with yml file

--- mycicdprojPROJ/mycicdprojAPP/models.py
+++ mycicdprojPROJ/mycicdprojAPP/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
+# Create your models here..
 class Post(models.Model):
 	title = models.CharField(max_length=255)
 	author = models.CharField(max_length=255)
